0c41fcb27eddbc8464d71bcb282dc4cac66cb2b5
Sander Dijkhuis <sander.dijkhuis@cleverbase.com>
2021-02-04 08:08:27 +0100

    wip: start making it reusable

--- src/main/scala/nl/sanderdijkhuis/hades/Signature.scala
+++ src/main/scala/nl/sanderdijkhuis/hades/Signature.scala
@@ -1,116 +1,24 @@
 package nl.sanderdijkhuis.hades
 
 import org.apache.xml.security.c14n.Canonicalizer
-import org.bouncycastle.asn1.oiw.OIWObjectIdentifiers
-import org.bouncycastle.asn1.x500.X500Name
-import org.bouncycastle.asn1.x509.{
-  AlgorithmIdentifier,
-  BasicConstraints,
-  Extension,
-  SubjectPublicKeyInfo
-}
-import org.bouncycastle.cert.X509ExtensionUtils
-import org.bouncycastle.cert.jcajce.{
-  JcaX509CertificateConverter,
-  JcaX509v3CertificateBuilder
-}
 import org.bouncycastle.crypto.params.RSAKeyParameters
 import org.bouncycastle.crypto.util.PublicKeyFactory
 import org.bouncycastle.jcajce.provider.digest.SHA3
-import org.bouncycastle.jce.provider.BouncyCastleProvider
-import org.bouncycastle.operator.ContentSigner
-import org.bouncycastle.operator.bc.BcDigestCalculatorProvider
-import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder
 import org.bouncycastle.util.encoders.Hex
-import org.w3c.dom.DOMImplementation
-import org.w3c.dom.ls.{
-  DOMImplementationLS,
-  LSInput,
-  LSOutput,
-  LSParser,
-  LSResourceResolver,
-  LSSerializer
-}
-import org.xml.sax.{ErrorHandler, InputSource, SAXParseException}
 
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream, StringReader}
-import java.math.BigInteger
-import java.net.{URI, URL}
+import java.io.ByteArrayOutputStream
+import java.net.URI
 import java.security.cert.X509Certificate
 import java.security.interfaces.RSAPrivateKey
 import java.security.spec.RSAPublicKeySpec
-import java.security.{Key, KeyFactory, MessageDigest, PrivateKey, SecureRandom}
-import java.time.temporal.ChronoUnit
-import java.util
-import java.util.{Base64, Date, UUID}
-import javax.xml.XMLConstants
-import javax.xml.bind.SchemaOutputResolver
-import javax.xml.crypto.dsig.SignatureMethod
-import javax.xml.crypto.{
-  AlgorithmMethod,
-  KeySelector,
-  KeySelectorException,
-  KeySelectorResult,
-  XMLCryptoContext,
-  XMLStructure
-}
-import javax.xml.crypto.dsig.dom.DOMValidateContext
-import javax.xml.crypto.dsig.keyinfo.{KeyInfo, X509Data}
-import javax.xml.parsers.DocumentBuilderFactory
-import javax.xml.transform.sax.SAXSource
-import javax.xml.transform.{Result, Source}
-import javax.xml.validation.SchemaFactory
+import java.security.{KeyFactory, MessageDigest}
+import java.util.Base64
 import scala.language.implicitConversions
-import scala.xml.{Elem, Node, NodeSeq, Text, TopScope, XML}
-import scala.jdk.CollectionConverters._
+import scala.xml.{Elem, Node, NodeSeq, Text}
 
 case class Signature()
 
 object Signature {
-  private val key: (SubjectPublicKeyInfo, ContentSigner, PrivateKey) = {
-    import java.security.KeyPairGenerator
-    val kpg = KeyPairGenerator.getInstance("RSA")
-    kpg.initialize(2048)
-    val kp = kpg.generateKeyPair()
-    val spki = SubjectPublicKeyInfo.getInstance(kp.getPublic.getEncoded)
-    val signer =
-      new JcaContentSignerBuilder("SHA256withRSA").build(kp.getPrivate)
-    (spki, signer, kp.getPrivate)
-  }
-
-  private val certificate: X509Certificate = {
-    val sr = new SecureRandom() // TODO
-    val serial = BigInteger.valueOf(sr.nextInt()) // TODO
-    val notBefore = new Date()
-    val notAfter = Date.from(notBefore.toInstant.plus(10, ChronoUnit.MINUTES))
-    val calc = new BcDigestCalculatorProvider()
-      .get(new AlgorithmIdentifier(OIWObjectIdentifiers.idSHA1))
-    val utils = new X509ExtensionUtils(calc)
-    val subjectKeyIdentifier = utils.createSubjectKeyIdentifier(key._1)
-    val authorityKeyIdentifier = utils.createAuthorityKeyIdentifier(key._1)
-    val basicConstraints = new BasicConstraints(false)
-    val name = new X500Name("CN=Test")
-    val params =
-      PublicKeyFactory.createKey(key._1).asInstanceOf[RSAKeyParameters]
-    val publicKey = KeyFactory
-      .getInstance("RSA")
-      .generatePublic(
-        new RSAPublicKeySpec(params.getModulus, params.getExponent))
-    val builder = new JcaX509v3CertificateBuilder(name,
-                                                  serial,
-                                                  notBefore,
-                                                  notAfter,
-                                                  name,
-                                                  publicKey)
-      .addExtension(Extension.subjectKeyIdentifier, false, subjectKeyIdentifier)
-      .addExtension(Extension.authorityKeyIdentifier,
-                    false,
-                    authorityKeyIdentifier)
-      .addExtension(Extension.basicConstraints, true, basicConstraints)
-    val converter =
-      new JcaX509CertificateConverter().setProvider(new BouncyCastleProvider)
-    converter.getCertificate(builder.build(key._2))
-  }
 
   org.apache.xml.security.Init.init()
 
@@ -191,7 +99,6 @@ object Signature {
   def generateReferenceToCurrentDocument(id: ReferenceId,
                                          node: Node): Reference = {
     val canonicalized = canonicalize(node)
-    println(s"canonicalized:${new String(canonicalized.value)}")
     Reference(
       Some(id),
       None,
@@ -201,9 +108,6 @@ object Signature {
       canonicalized.digestValue
     )
   }
-
-  val params: RSAKeyParameters =
-    PublicKeyFactory.createKey(key._1).asInstanceOf[RSAKeyParameters]
 
   case class DigitalSignature(id: SignatureId,
                               signedInfo: SignedInfo,
@@ -313,136 +217,14 @@ object Signature {
 
   case class SignatureValue(value: Array[Byte])
 
-  // TODO https://www.oracle.com/technical-resources/articles/java/dig-signature-api.html
-  def validate(signature: Elem): Unit = {
-    val dbf = DocumentBuilderFactory.newInstance()
-    dbf.setNamespaceAware(true)
-    val is = new InputSource(new StringReader(signature.toString()))
-
-    val schemaFactory =
-      SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-    println(
-      "Old resolver, could be used for default behavior: " + schemaFactory.getResourceResolver)
-    schemaFactory.setResourceResolver(new LSResourceResolver {
-      override def resolveResource(`type`: String,
-                                   namespaceURI: String,
-                                   publicId: String,
-                                   systemId: String,
-                                   baseURI: String): LSInput = systemId match {
-        case "http://www.w3.org/TR/2002/REC-xmldsig-core-20020212/xmldsig-core-schema.xsd" => {
-          val domImpl = DocumentBuilderFactory
-            .newInstance()
-            .newDocumentBuilder()
-            .getDOMImplementation
-            .getFeature("LS", "3.0")
-            .asInstanceOf[DOMImplementationLS]
-          val input = domImpl.createLSInput()
-          val path = URI.create(systemId).getPath
-          input.setPublicId(publicId)
-          input.setBaseURI(baseURI)
-          input.setSystemId(systemId)
-          input.setByteStream(
-            getClass.getResourceAsStream("xmldsig-core-schema.xsd"))
-          input
-//          val dols = domImpl.createLSInput()
-//          val in =
-//            dols.setCharacterStream(in)
-        }
-        case id => {
-          println("Could not find " + id)
-          null
-        }
-//        println("resolving")
-//        println(`type`)
-//        println(namespaceURI)
-//        println(publicId)
-//        println(systemId)
-//        println(baseURI)
-      }
-    })
-
-    val schemaPath = getClass.getResource("/XAdES.xsd")
-    val schema = schemaFactory.newSchema(schemaPath)
-//    val schema = schemaFactory.newSchema(
-//      new URL("https://uri.etsi.org/01903/v1.3.2/XAdES.xsd"))
-    // import javax.xml.transform.stream.StreamSource
-//    val schema: Nothing = sf.newSchema(new StreamSource(getClass.getResourceAsStream(SCHEMA_PATH)))
-    dbf.setSchema(schema)
-
-    val builder = dbf.newDocumentBuilder()
-    builder.setErrorHandler(new ErrorHandler {
-      override def warning(exception: SAXParseException): Unit = ???
-
-      override def error(exception: SAXParseException): Unit =
-        println("exception" + exception)
-
-      override def fatalError(exception: SAXParseException): Unit = ???
-    })
-    val doc = builder.parse(is)
-    println("elem" + doc.getDocumentElement)
-//    doc.getDocumentElement.setIdAttribute("Id", true)
-
-    val valContext = new DOMValidateContext(
-      new KeySelector {
-        override def select(keyInfo: KeyInfo,
-                            purpose: KeySelector.Purpose,
-                            method: AlgorithmMethod,
-                            context: XMLCryptoContext): KeySelectorResult = {
-          keyInfo.getContent
-            .iterator()
-            .asScala
-            .collect {
-              case d: X509Data => {
-                d.getContent.iterator().asScala.collect {
-                  case c: X509Certificate
-                      if c.getPublicKey.getAlgorithm
-                        .equalsIgnoreCase("RSA") && method.getAlgorithm
-                        .equalsIgnoreCase(SignatureMethod.RSA_SHA256) => {
-                    c.getPublicKey
-                  }
-                }
-              }
-            }
-            .flatten
-            .nextOption() match {
-            case Some(key) =>
-              new KeySelectorResult {
-                override def getKey: Key = key
-              }
-            case _ => throw new KeySelectorException("No key found")
-          }
-        }
-      },
-      doc
-        .getElementsByTagNameNS("http://www.w3.org/2000/09/xmldsig#",
-                                "Signature")
-        .item(0)
-    )
-    import javax.xml.crypto.dsig.XMLSignatureFactory
-    val fac = XMLSignatureFactory.getInstance("DOM")
-    val s = fac.unmarshalXMLSignature(valContext)
-    val valid = s.validate(valContext)
-    println("valid?" + valid)
-  }
-
-  // TODO factor preparation out of this function, make a separate sign() function
-  def prepareDataToBeSigned(document: Elem): Elem = {
-
-//    def stripNamespaces(node: Node): Node = node match {
-//      case e: Elem =>
-//        e.copy(scope = TopScope, child = e.child map stripNamespaces)
-//      case _ => node
-//    }
-
-    // https://www.etsi.org/deliver/etsi_ts/101900_101999/101903/01.04.02_60/ts_101903v010402p.pdf
-    // https://www.w3.org/TR/xmldsig-core1/#sec-Processing
-
+  def analyzeDocument(document: Elem)
+    : (SignatureId, List[Reference], List[DigitalSignatureObject]) = {
     // Hashing not for security but for identification
     val digest = new SHA3.Digest256()
     digest.update(document.toString().getBytes)
     // TODO add certificates
 
-//    val id = Base64.getUrlEncoder.encodeToString(digest.digest())
+    //    val id = Base64.getUrlEncoder.encodeToString(digest.digest())
     val id = new String(Hex.encode(digest.digest())).substring(0, 16)
 
     val signatureId = SignatureId(s"sig-id-${id}")
@@ -457,17 +239,9 @@ object Signature {
       DigitalSignatureObject(
         QualifyingProperties(signatureId, xadesSignedProperties))
     )
-
     val alteredDocument = document
       .copy(child = document.child ++ Text("\n") ++ Text("\n\n"))
-
-    println("original" + document.child.toList)
-    println("altered" + alteredDocument.child.toList)
-
-    println(
-      "xades" + new String(canonicalize(xadesSignedProperties.toXml).value))
-
-    val references =
+    val references = {
       List(
         generateReferenceToCurrentDocument(documentReferenceId,
                                            alteredDocument),
@@ -479,35 +253,42 @@ object Signature {
           canonicalize(xadesSignedProperties.toXml).digestValue
         )
       )
+    }
+    (signatureId, references, objects)
+  }
 
-    val dataToBeSigned = originalDataToBeSigned(references)
-    val privateKey = key._3.asInstanceOf[RSAPrivateKey]
+  // TODO factor preparation out of this function, make a separate sign() function
+  def prepareDataToBeSigned(document: Elem): OriginalDataToBeSigned = {
 
-    val sig = java.security.Signature.getInstance("SHA256withRSA")
-    sig.initSign(privateKey)
-    sig.update(dataToBeSigned.value)
-    val signature = SignatureValue(sig.sign())
+    //    def stripNamespaces(node: Node): Node = node match {
+    //      case e: Elem =>
+    //        e.copy(scope = TopScope, child = e.child map stripNamespaces)
+    //      case _ => node
+    //    }
 
+    // https://www.etsi.org/deliver/etsi_ts/101900_101999/101903/01.04.02_60/ts_101903v010402p.pdf
+    // https://www.w3.org/TR/xmldsig-core1/#sec-Processing
+
+    val (_, references, _) = analyzeDocument(document)
+    originalDataToBeSigned(references)
+  }
+
+  def signDocument(document: Elem,
+                   certificate: X509Certificate,
+                   params: RSAKeyParameters, // TODO get from certificate
+                   signature: SignatureValue): Elem = {
+    val (signatureId, references, objects) = analyzeDocument(document)
+    val dataToBeSigned = prepareDataToBeSigned(document)
     val sig2 = java.security.Signature.getInstance("SHA256withRSA")
-//    val params =
-//      PublicKeyFactory.createKey(key._1).asInstanceOf[RSAKeyParameters]
     val publicKey = KeyFactory
       .getInstance("RSA")
       .generatePublic(
         new RSAPublicKeySpec(params.getModulus, params.getExponent))
     sig2.initVerify(publicKey)
     sig2.update(dataToBeSigned.value)
-    println("result:" + sig2.verify(signature.value))
-//    val x = privateKey
-//
-//    val signer = key._2
-//    val out = signer.getOutputStream
-//    println(signer.getAlgorithmIdentifier.getAlgorithm.toString)
-////    out.write(dataToBeSigned.value)
-//    out.write(MessageDigest.getInstance("SHA-256").digest(dataToBeSigned.value))
-//    out.close()
-//    val signature = SignatureValue(signer.getSignature)
-    println("sign:" + new String(dataToBeSigned.value))
+
+    if (!sig2.verify(signature.value))
+      throw new Exception("Invalid signature value")
 
     val d = document.copy(
       child = document.child ++ Text("\n") ++ sign(signatureId,
@@ -516,10 +297,6 @@ object Signature {
                                                    certificate,
                                                    objects,
       ).toXml ++ Text("\n\n"))
-
-    println(d)
-
-    validate(d)
 
     d
   }
--- src/test/scala/nl/sanderdijkhuis/hades/SignatureSpec.scala
+++ src/test/scala/nl/sanderdijkhuis/hades/SignatureSpec.scala
@@ -1,11 +1,46 @@
 package nl.sanderdijkhuis.hades
 
-import nl.sanderdijkhuis.hades.Signature.SignatureId
+import org.bouncycastle.asn1.oiw.OIWObjectIdentifiers
+import org.bouncycastle.asn1.x500.X500Name
+import org.bouncycastle.asn1.x509.{
+  AlgorithmIdentifier,
+  BasicConstraints,
+  Extension,
+  SubjectPublicKeyInfo
+}
+import org.bouncycastle.cert.X509ExtensionUtils
+import org.bouncycastle.cert.jcajce.{
+  JcaX509CertificateConverter,
+  JcaX509v3CertificateBuilder
+}
+import org.bouncycastle.crypto.params.RSAKeyParameters
+import org.bouncycastle.crypto.util.PublicKeyFactory
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.operator.ContentSigner
+import org.bouncycastle.operator.bc.BcDigestCalculatorProvider
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder
 import org.scalatest._
 import org.scalatest.featurespec.AnyFeatureSpec
+import org.w3c.dom.ls.{DOMImplementationLS, LSInput, LSResourceResolver}
+import org.xml.sax.{ErrorHandler, InputSource, SAXParseException}
 
-import java.util.UUID
-import scala.xml.PrettyPrinter
+import java.io.StringReader
+import java.math.BigInteger
+import java.security.cert.X509Certificate
+import java.security.interfaces.RSAPrivateKey
+import java.security.spec.RSAPublicKeySpec
+import java.security.{Key, KeyFactory, PrivateKey, SecureRandom}
+import java.time.temporal.ChronoUnit
+import java.util.Date
+import javax.xml.XMLConstants
+import javax.xml.crypto._
+import javax.xml.crypto.dsig.{SignatureMethod, XMLSignatureFactory}
+import javax.xml.crypto.dsig.dom.DOMValidateContext
+import javax.xml.crypto.dsig.keyinfo.{KeyInfo, X509Data}
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.validation.SchemaFactory
+import scala.jdk.CollectionConverters._
+import scala.xml.Elem
 
 class SignatureSpec extends AnyFeatureSpec with GivenWhenThen {
   Feature("Signatures") {
@@ -19,12 +54,159 @@ class SignatureSpec extends AnyFeatureSpec with GivenWhenThen {
       When("I prepare the document for signing")
       val dataToBeSigned = Signature.prepareDataToBeSigned(doc)
 
-//      val p = new PrettyPrinter(120, 2)
-//
-//      println(p.format(dataToBeSigned))
+      And("I sign the document")
+      val privateKey = testKey._3.asInstanceOf[RSAPrivateKey]
 
-      println()
-      println(dataToBeSigned.toString())
+      val sig = java.security.Signature.getInstance("SHA256withRSA")
+      sig.initSign(privateKey)
+      sig.update(dataToBeSigned.value)
+      val signatureValue = Signature.SignatureValue(sig.sign())
+      val signature =
+        Signature.signDocument(doc, testCertificate, testParams, signatureValue)
+
+      println(signature)
     }
+  }
+
+  private val testKey: (SubjectPublicKeyInfo, ContentSigner, PrivateKey) = {
+    import java.security.KeyPairGenerator
+    val kpg = KeyPairGenerator.getInstance("RSA")
+    kpg.initialize(2048)
+    val kp = kpg.generateKeyPair()
+    val spki = SubjectPublicKeyInfo.getInstance(kp.getPublic.getEncoded)
+    val signer =
+      new JcaContentSignerBuilder("SHA256withRSA").build(kp.getPrivate)
+    (spki, signer, kp.getPrivate)
+  }
+
+  private val testCertificate: X509Certificate = {
+    val sr = new SecureRandom() // TODO
+    val serial = BigInteger.valueOf(sr.nextInt()) // TODO
+    val notBefore = new Date()
+    val notAfter = Date.from(notBefore.toInstant.plus(10, ChronoUnit.MINUTES))
+    val calc = new BcDigestCalculatorProvider()
+      .get(new AlgorithmIdentifier(OIWObjectIdentifiers.idSHA1))
+    val utils = new X509ExtensionUtils(calc)
+    val subjectKeyIdentifier = utils.createSubjectKeyIdentifier(testKey._1)
+    val authorityKeyIdentifier = utils.createAuthorityKeyIdentifier(testKey._1)
+    val basicConstraints = new BasicConstraints(false)
+    val name = new X500Name("CN=Test")
+    val params =
+      PublicKeyFactory.createKey(testKey._1).asInstanceOf[RSAKeyParameters]
+    val publicKey = KeyFactory
+      .getInstance("RSA")
+      .generatePublic(
+        new RSAPublicKeySpec(params.getModulus, params.getExponent))
+    val builder = new JcaX509v3CertificateBuilder(name,
+                                                  serial,
+                                                  notBefore,
+                                                  notAfter,
+                                                  name,
+                                                  publicKey)
+      .addExtension(Extension.subjectKeyIdentifier, false, subjectKeyIdentifier)
+      .addExtension(Extension.authorityKeyIdentifier,
+                    false,
+                    authorityKeyIdentifier)
+      .addExtension(Extension.basicConstraints, true, basicConstraints)
+    val converter =
+      new JcaX509CertificateConverter().setProvider(new BouncyCastleProvider)
+    converter.getCertificate(builder.build(testKey._2))
+  }
+
+  val testParams: RSAKeyParameters =
+    PublicKeyFactory.createKey(testKey._1).asInstanceOf[RSAKeyParameters]
+
+  // See https://www.oracle.com/technical-resources/articles/java/dig-signature-api.html
+  def validateSignature(signature: Elem): Boolean = {
+    val dbf = DocumentBuilderFactory.newInstance()
+    dbf.setNamespaceAware(true)
+    val is = new InputSource(new StringReader(signature.toString()))
+
+    val schemaFactory =
+      SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+    println(
+      "Old resolver, could be used for default behavior: " + schemaFactory.getResourceResolver)
+    schemaFactory.setResourceResolver(new LSResourceResolver {
+      override def resolveResource(`type`: String,
+                                   namespaceURI: String,
+                                   publicId: String,
+                                   systemId: String,
+                                   baseURI: String): LSInput = systemId match {
+        case "http://www.w3.org/TR/2002/REC-xmldsig-core-20020212/xmldsig-core-schema.xsd" => {
+          val domImpl = DocumentBuilderFactory
+            .newInstance()
+            .newDocumentBuilder()
+            .getDOMImplementation
+            .getFeature("LS", "3.0")
+            .asInstanceOf[DOMImplementationLS]
+          val input = domImpl.createLSInput()
+          input.setPublicId(publicId)
+          input.setBaseURI(baseURI)
+          input.setSystemId(systemId)
+          input.setByteStream(
+            getClass.getResourceAsStream("xmldsig-core-schema.xsd"))
+          input
+        }
+        case id => {
+          println("Could not find " + id)
+          null
+        }
+      }
+    })
+
+    val schemaPath = getClass.getResource("/XAdES.xsd")
+    val schema = schemaFactory.newSchema(schemaPath)
+    dbf.setSchema(schema)
+
+    val builder = dbf.newDocumentBuilder()
+    builder.setErrorHandler(new ErrorHandler {
+      override def warning(exception: SAXParseException): Unit = ???
+
+      override def error(exception: SAXParseException): Unit =
+        println("exception" + exception)
+
+      override def fatalError(exception: SAXParseException): Unit = ???
+    })
+    val doc = builder.parse(is)
+
+    val valContext = new DOMValidateContext(
+      new KeySelector {
+        override def select(keyInfo: KeyInfo,
+                            purpose: KeySelector.Purpose,
+                            method: AlgorithmMethod,
+                            context: XMLCryptoContext): KeySelectorResult = {
+          keyInfo.getContent
+            .iterator()
+            .asScala
+            .collect {
+              case d: X509Data => {
+                d.getContent.iterator().asScala.collect {
+                  case c: X509Certificate
+                      if c.getPublicKey.getAlgorithm
+                        .equalsIgnoreCase("RSA") && method.getAlgorithm
+                        .equalsIgnoreCase(SignatureMethod.RSA_SHA256) => {
+                    c.getPublicKey
+                  }
+                }
+              }
+            }
+            .flatten
+            .nextOption() match {
+            case Some(key) =>
+              new KeySelectorResult {
+                override def getKey: Key = key
+              }
+            case _ => throw new KeySelectorException("No key found")
+          }
+        }
+      },
+      doc
+        .getElementsByTagNameNS("http://www.w3.org/2000/09/xmldsig#",
+                                "Signature")
+        .item(0)
+    )
+    val fac = XMLSignatureFactory.getInstance("DOM")
+    val s = fac.unmarshalXMLSignature(valContext)
+    s.validate(valContext)
   }
 }
